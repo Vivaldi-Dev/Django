f1022d846f641b985e84429a8d4e10ecbdb9f75e
domvivas123 <nobelavivaldi@gmail.com>
2024-06-23 19:08:19 +0200

    GET e POST e PUT

--- CrudApi/api/urls.py
+++ CrudApi/api/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('',views.get_users, name='get_all_users'),
     path('users/<str:nick>/', views.get_nick, name='get_nick_name'),
+    path('data/',views.get_all),
+    path('post/',views.post_data)
     
 ]
--- CrudApi/api/views.py
+++ CrudApi/api/views.py
@@ -8,6 +8,8 @@ from rest_framework import status
 from .models import User
 from .serializers import UserSerializers
 
+
+# GET all
 @api_view(['GET'])
 def get_users(request):
     if request.method == 'GET':
@@ -16,7 +18,7 @@ def get_users(request):
         return Response(serializers.data)
     
     return Response(status=status.HTTP_400_BAD_REQUEST)
-
+# GET By name
 @api_view(['GET'])
 def get_nick(request, nick):
     try:
@@ -27,5 +29,52 @@ def get_nick(request, nick):
     if request.method == 'GET':
         serializers = UserSerializers(users)   
         return Response(serializers.data) 
-   
-   
+    
+# GET com parametros   
+@api_view (['GET'])
+def get_all(request): 
+    
+    if request.method == 'GET':
+        
+        try:
+            if request.GET['user']:
+                user_nickname = request.GET['user']
+                try:
+                    user = User.objects.get(pk=user_nickname)
+                except:
+                    return Response (status=status.HTTP_404_NOT_FOUND)
+                serializers = UserSerializers(user)
+                return Response(serializers.data)
+                
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)            
+
+@api_view(['POST'])
+def post_data(request):
+    if request.method == 'POST':
+        new_user = request.data 
+        serializers = UserSerializers(data=new_user)
+        
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST )
+    
+@api_view (['GET'])
+def put_data(request):
+    if request.method == 'PUT':
+        nickname = request.data['user_nickname']   
+        
+        update_user = User.objects.get(pk=nickname)
+        
+        print(request.data)
+        
+        serializers = UserSerializers(update_user,data=request.data)
+        
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_400_BAD_REQUEST) 
